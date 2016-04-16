4fa3da13beb04a1f2f6a5baa4d8ca7b12fc83f12
Ajith KP <ajithkp1892@gmail.com>
2016-04-17 02:50:37 +0530

    Update db.php

--- phpScripts/db.php
+++ phpScripts/db.php
@@ -2,7 +2,7 @@
 echo "<script>console.log('Entered');</script>";
 define('DB_SERVER', 'mysql.hostinger.in');
 define('DB_USERNAME', 'u990132261_notif');
-define('DB_PASSWORD', 'bmsit-notif');
+define('DB_PASSWORD', 'bmsitnotif');
 define('DB_DATABASE', 'u990132261_bmsit');
 
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);

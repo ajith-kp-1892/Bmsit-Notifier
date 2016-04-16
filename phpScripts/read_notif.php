3b5ee6f59089d730024aebb26735b07746cdd4cd
Ajith KP <ajithkp1892@gmail.com>
2016-04-17 03:23:04 +0530

    Update read_notif.php

--- phpScripts/read_notif.php
+++ phpScripts/read_notif.php
@@ -5,19 +5,18 @@ include 'db.php';
 
 $fetchNotifications = "select * from notification";
 $execFetchNotifications = mysqli_query($db,$fetchNotifications);
-$rows = mysqli_num_rows($db,$execFetchNotifications);
+$rows = mysqli_num_rows($execFetchNotifications);
 
 if ($rows > 0) {
 	$count = 0;
 	$data = array();
-    while($row = mysqli_fetch_assoc($db,$execFetchNotifications)) {         
+    while($row = mysqli_fetch_assoc($execFetchNotifications)) {         
 		$data[$count++] = $row;
     }
 	header('Content-Type: application/json');
-	echo json_encode($rows);
+	echo json_encode($data);
 	
 } else {
     echo "No results found";
 }
-
-mysqli_
+?>

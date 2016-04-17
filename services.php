13f55d3850b469226b93b2347ea1c8d379bb23c7
Ajith KP <ajithkp1892@gmail.com>
2016-04-17 22:43:27 +0530

    Update services.php

--- services.php
+++ services.php
@@ -49,9 +49,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 					<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
 						<nav class="cl-effect-13" id="cl-effect-13">
 							<ul class="nav navbar-nav">
-								<li><a href="adminCreateEvents.php" class="active">Event Creation</a></li>
-								<li><a href="adminCreateNotification.php">Notification Creation</a></li>
-								<li><a href="events.php">Events</a></li>
+								<li><a href="events.php" class="active">Events</a></li>
 								<li><a href="services.php">Notification</a></li>
 								<li><a href="phpScripts/logout.php">Logout</a></li>
 							</ul>

9c8e54966709f20f28fd2d702f701d0481d7710d
Chinmay <chinmaybhat1@gmail.com>
2016-07-20 12:40:02 +0530

    Update db.php

--- phpScripts/db.php
+++ phpScripts/db.php
@@ -1,15 +1,14 @@
 <?php
-//echo "<script>console.log('Entered');</script>";
+//Initialize DB parameters
 define('DB_SERVER', 'mysql.hostinger.in');
 define('DB_USERNAME', 'u990132261_notif');
 define('DB_PASSWORD', 'bmsitnotif');
 define('DB_DATABASE', 'u990132261_bmsit');
-
+//Connect to server DB
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-
+//Error handling connectivity
 if (!$db) {
 	echo "Unable to connect to DB: " . mysqli_connect_error();
 	exit;
 }
-
 ?>
